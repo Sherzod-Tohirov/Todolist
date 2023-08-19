005382230bfa06f85864d1d22cd5a0f11d244b6e
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-08-19 13:34:58 +0500

    Add new features

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ let elInpTime = document.querySelector('.js-time');
 let elBtn = document.querySelector('.js-btn');
 let elList = document.querySelector('.wrapper__list');
 let clrBtn = document.querySelector('.js-clear');
+const elMicBtn = document.querySelector('.js-mic-btn');
+const elMicImg = document.querySelector('.js-mic-img');
+const webkitSpeech = new webkitSpeechRecognition();
 if (localStorage.getItem('items')) {
   let liVal = localStorage.getItem('items');
   elList.innerHTML = liVal;
@@ -16,49 +19,23 @@ elInp.addEventListener('input', (evt) => {
 elInpTime.addEventListener('input', (evt) => {
   check(elInpTime, elInp, elBtn);
 });
-  console.log(elList.childNodes.length);
+console.log(elList.childNodes.length);
 
 elForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  let inpVal = elInp.value;
-  let inpTimeVal = elInpTime.value;
-  let elLi = document.createElement('li');
-  let elp = document.createElement('p');
-  let elLiBtn = document.createElement('button');
-  let elTime = document.createElement('time');
-  elLi.classList.add('wrapper__item');
-  elp.classList.add('item__desc');
-  elp.innerHTML = inpVal;
-  elTime.classList.add('item__time');
-  elTime.innerHTML = inpTimeVal;
-  elp.appendChild(elTime);
-  elLiBtn.classList.add('item__btn');
-  elLiBtn.innerHTML = "&times;";
-  elLi.appendChild(elp);
-  elLi.appendChild(elLiBtn);
-  elList.appendChild(elLi)
-  elInp.value = '';
-  elInpTime.value = '';
-  elBtn.style.backgroundColor = 'gray';
-  elBtn.setAttribute('disabled', '');
-  localStorage.setItem('items', elList.innerHTML);
-  if (elList.childNodes.length == 0) {
-    clrBtn.style.display = 'none';
-  } else {
-    clrBtn.style.display = 'flex';
-  }
+  addToDo(elInp.value, elInpTime.value);
 });
 
 elList.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('item__desc') || evt.target.classList.contains('item__time')) {
-    if(!evt.target.classList.contains('item__time')) {
-       evt.target.parentElement.classList.toggle('wrapper__item--active');
-       evt.target.classList.toggle('item__desc--active');
-    }else {
-       evt.target.closest('.wrapper__item').classList.toggle('wrapper__item--active');
-       evt.target.parentElement.classList.toggle('item__desc--active');
+    if (!evt.target.classList.contains('item__time')) {
+      evt.target.parentElement.classList.toggle('wrapper__item--active');
+      evt.target.classList.toggle('item__desc--active');
+    } else {
+      evt.target.closest('.wrapper__item').classList.toggle('wrapper__item--active');
+      evt.target.parentElement.classList.toggle('item__desc--active');
     }
-    
+
   }
   if (evt.target.classList.contains('item__btn')) {
     evt.target.parentElement.remove();
@@ -84,6 +61,39 @@ clrBtn.addEventListener('click', (evt) => {
   evt.target.remove();
 });
 
+
+elMicBtn.addEventListener('click', (evt) => {
+  webkitSpeech.start();
+
+  webkitSpeech.onresult = (evt) => {
+    let val = evt.results[0][0].transcript;
+    if (val.length > 0 || val !== undefined) {
+      let todo = val.substr(0, val.indexOf('at'));
+      let time = val.substr(val.indexOf('at') + 2);
+      if (time.length > 0) {
+        if (todo && time) {
+          addToDo(todo, time);
+        } else {
+          console.log("Something went wrong !");
+        }
+      }
+    }
+  }
+
+  webkitSpeech.onstart = (evt) => {
+    elMicImg.classList.add('wrapper__img--active');
+  }
+
+
+  webkitSpeech.onend = (evt) => {
+    elMicImg.classList.remove('wrapper__img--active');
+  }
+
+});
+
+
+
+
 function check(element, second_element, btn_element) {
   if (element.value.length == 0 || (/^\s/.test(element.value) || second_element.value.length == 0)) {
     btn_element.style.backgroundColor = 'gray';
@@ -92,5 +102,33 @@ function check(element, second_element, btn_element) {
   } else {
     btn_element.style.backgroundColor = 'rgb(255, 100, 60)';
     btn_element.removeAttribute('disabled');
+  }
+}
+
+function addToDo(inp, time) {
+  let elLi = document.createElement('li');
+  let elp = document.createElement('p');
+  let elLiBtn = document.createElement('button');
+  let elTime = document.createElement('time');
+  elLi.classList.add('wrapper__item');
+  elp.classList.add('item__desc');
+  elp.innerHTML = inp;
+  elTime.classList.add('item__time');
+  elTime.innerHTML = time;
+  elp.appendChild(elTime);
+  elLiBtn.classList.add('item__btn');
+  elLiBtn.innerHTML = "&times;";
+  elLi.appendChild(elp);
+  elLi.appendChild(elLiBtn);
+  elList.appendChild(elLi)
+  elInp.value = '';
+  elInpTime.value = '';
+  elBtn.style.backgroundColor = 'gray';
+  elBtn.setAttribute('disabled', '');
+  localStorage.setItem('items', elList.innerHTML);
+  if (elList.childNodes.length == 0) {
+    clrBtn.style.display = 'none';
+  } else {
+    clrBtn.style.display = 'flex';
   }
 }
